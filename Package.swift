b0d773ff1441bad7da274e5039ce193c59ea613e
Guillermo Cique <guillermo@holvi.com>
2020-11-16 14:48:00 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,17 +8,17 @@ let package = Package(
         .library(name: "ChatSDK", targets: ["ChatSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/GuillermoCiqueHolvi/sdkconfigurations_sdk_ios", .upToNextMajor(from: "1.1.6")),
-        .package(url: "https://github.com/GuillermoCiqueHolvi/commonui_sdk_ios", .upToNextMajor(from: "6.0.0")),
-        .package(url: "https://github.com/GuillermoCiqueHolvi/messaging_sdk_ios", .upToNextMajor(from: "3.8.0")),
-        .package(url: "https://github.com/GuillermoCiqueHolvi/messagingapi_sdk_ios", .upToNextMajor(from: "3.8.0")),
-        .package(url: "https://github.com/GuillermoCiqueHolvi/chat_providers_sdk_ios", .upToNextMajor(from: "2.9.0"))
+        .package(url: "https://github.com/GuillermoCiqueHolvi/sdkconfigurations_sdk_ios", .upToNextMajor(from: "1.1.7")),
+        .package(url: "https://github.com/GuillermoCiqueHolvi/commonui_sdk_ios", .upToNextMajor(from: "6.0.1")),
+        .package(url: "https://github.com/GuillermoCiqueHolvi/messaging_sdk_ios", .upToNextMajor(from: "3.8.1")),
+        .package(url: "https://github.com/GuillermoCiqueHolvi/messagingapi_sdk_ios", .upToNextMajor(from: "3.8.1")),
+        .package(url: "https://github.com/GuillermoCiqueHolvi/chat_providers_sdk_ios", .upToNextMajor(from: "2.9.1"))
     ],
     targets: [
         .binaryTarget(
             name: "ChatSDK",
-            url: "https://zendesk.jfrog.io/zendesk/libs-releases-local/ios/zendesk/5.2.4/ChatSDK/2.9.0/universal/ChatSDK.zip",
-            checksum: "d0ed80acb62790ac3475b16cb01a677f48b3dcbbe503de244afe4e7a99bb4358"
+            url: "https://github.com/GuillermoCiqueHolvi/chat_sdk_ios/archive/2.9.1.zip",
+            checksum: "8e7aed62fca70030f6cd7c76f389070ab7f0a31c3fea3c2ed9f2db6757af4d59"
         )
     ]
 )
